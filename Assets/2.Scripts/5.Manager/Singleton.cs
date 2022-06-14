261097cfaf6d39570631856e4bf7df82176d2e92
Hyeonjun <wns8363@gmail.com>
2022-06-14 21:05:35 +0900

    [Feat] 싱글톤 스크립트 추가
    매니저 클래스를 싱글톤으로 사용하기 위한 스크립트

---
+++ Assets/2.Scripts/5.Manager/Singleton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+
+    protected static T _instance;
+    public static T Instance
+    {
+        get { return _instance; }
+    }
+}
