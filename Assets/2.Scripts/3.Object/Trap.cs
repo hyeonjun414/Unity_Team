6293de1baf232c0b73941b6284cd0201baccd28d
Charlotte Nam <82144509+CharlotteGracy@users.noreply.github.com>
2022-06-15 14:39:43 +0900

    [Feat] 함정 및 아이템 구조 설계
    Potion, Trap 스크립트 추가.

---
+++ Assets/2.Scripts/3.Object/Trap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+      private Character cplayer;
+
+      void private void OnCollisionEnter(Collision other) {
+         if(other.gameObject.name == "Player"){
+            var contactedPlayer = collision.gameObject;
+            Debug.Log(contactedPlayer + "랑 충돌!")
+        }
+    }
+
+
+   //플레이어 라이프 감소
+
+   //가시 트랩: 해당 블럭 진입시 라이프 1개 감소
+   public void ThornTrap(){
+      cplayer.hp = cplayer.hp - 1;
+
+   }
+
+
+   //얼음 트랩: 해당 블럭 진입시 한턴 동안 마비
+   public void IceTrap(){
+
+
+
+   }
+
+
+
+
+
+
+}
