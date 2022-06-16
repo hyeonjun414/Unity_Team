16e463677e4cccb83a25a0d97b69180f47844fed
Charlotte Nam <82144509+CharlotteGracy@users.noreply.github.com>
2022-06-16 17:22:55 +0900

    [Feat] 플레이어와 아이템 충돌 시 반응 구현

--- Assets/2.Scripts/3.Object/Item.cs
+++ Assets/2.Scripts/3.Object/Item.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public ItemData data;
-    public int holdingTime = 5;
+    //public int holdingTime = 5;
 
-    private void OnCollisionEnter(Collision other) {
+    private void OnTriggerEnter(Collider other){
 
         //아이템과 충돌한 플레이어 알려주기
         if(other.gameObject.tag == "Player"){
             var contactedPlayer = other.gameObject.GetComponent<Character>();
             Debug.Log(contactedPlayer + "랑 충돌!");
-            //플레이어와 충돌한 아이템 삭제
            // contactedPlayer.AddItem(itemData)
-            //Destroy(gameObject);
+
+            //플레이어와 충돌한 아이템 삭제
+            Destroy(gameObject);
         }
     }
 
--- Assets/2.Scripts/5.Manager/ItemManager.cs
+++ Assets/2.Scripts/5.Manager/ItemManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿//아이템 매니저: 플레이어와 아이템이 충돌했을 때 아이템 데이터에 맞는 행동을 보내줌.
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -19,6 +21,13 @@ public void UseItem(Character player, ItemData data){
         case ItemType.HEAL:
             HealPotion(player);
             break; 
+        case ItemType.POWERUP:
+            break;
+        case ItemType.DASH:
+            break;
+        case ItemType.SEEINGTHORUGH:
+            break;
+
 
     }
 
@@ -28,35 +37,47 @@ public void UseItem(Character player, ItemData data){
        
         //라이프가 2개 늘어남
         player.characterStatus.hp = 2 + player.characterStatus.hp;
+
+        //테스트용 디버그 로그
+        Debug.Log("플레이어의 체력이 2 증가합니다.");
+
         //늘어난 라이프가 5개 이상일 경우 5개로 고정해줌
         if(player.characterStatus.hp > 5){
             player.characterStatus.hp = 5;
 
+            //테스트용 디버그 로그
+            Debug.Log("플레이어의 체력이 이미 최대입니다!");
+
         }
+
     }
 
 
-    public void PowerUpPotion(){
+    public void PowerUpPotion(Character player){
         //공격력이 2배로 증가
+        Debug.Log("공격력이 두 배로 증가합니다.");
 
 
 
 
     }
 
-    public void DashItem(){
+    public void DashItem(Character player){
         //같은 이동을 한 턴에 연속 두번 처리
         //이동 종류: 오른쪽 회전, 왼쪽 회전, 앞, 뒤, 좌, 우
         //앞뒤좌우 이동 시 같은 방향으로 2칸 이동, Vector로는 2씩 이동
 
         //현 아이템이 사용 되면
+        Debug.Log("이동이 두 배로 증가합니다.");
 
 
     }
 
-    public void SeeingThrough(){
+    public void SeeingThrough(Character player){
         //벽 투시
         //오브젝트 알파값
+        Debug.Log("벽을 투시합니다.");
+
     }
 
 
