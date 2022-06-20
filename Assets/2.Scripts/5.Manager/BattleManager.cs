5ca47567404fabc0972ab0f44d14bbdb27cbe80b
Charlotte Nam <82144509+CharlotteGracy@users.noreply.github.com>
2022-06-20 20:11:28 +0900

    [Feat] 그라운드 종료 시 승패 결과 텍스트 코드 구현

--- Assets/2.Scripts/5.Manager/BattleManager.cs
+++ Assets/2.Scripts/5.Manager/BattleManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Pun.UtilityScripts;
 using Photon.Realtime;
@@ -18,15 +19,42 @@ public class BattleManager : MonoBehaviourPun
 
     public List<Character> players;
 
+
+    [Header("Player")]
+   
+    //살아있는 플레이어 
+    public List<Character> alivePlayer;
+    //사망한 플레이어
+    public List<Character> deadPlayer;
+
+
+    [Header("Text")]
+
+    public Text resultText;
+
+
+
     public static BattleManager Instance {get;private set;}
     private void Awake()
     {
         if (Instance == null)  Instance = this;
    
     }
+
+
+    private void Update() {
+        FinalWinner();
+    }
+
+
+
     public void RegisterAllPlayer()
     {
         players = FindObjectsOfType<Character>().ToList();
+
+        //게임이 시작했을 때 들어온 모든 플레이어를 살아있는 플레이어 그룹에 넣는다.
+        alivePlayer = FindObjectsOfType<Character>().ToList();
+      
         players.Sort((Character a, Character b) =>
         {
             if (a.playerNumber < b.playerNumber)
@@ -119,9 +147,33 @@ public class BattleManager : MonoBehaviourPun
 
 
 
+    //플레이어가 죽었을 때 판정
+    public void PlayerOut(Character deadPL){
+
+        //alivePlayer 리스트에서 죽은 플레이어를 뺀다.
+        alivePlayer.Remove(deadPL);
+        //deadPlayer 리스트에 죽은 플레이어를 더해준다.
+        deadPlayer.Add(deadPL);
+    }
 
 
-
-
+    public void BattleOverMessage(){
+        //플레이어가 죽지 않았을 때
+        foreach(Character player in alivePlayer){
+            resultText.text = "YOU WIN!";
+       }
+        //플레이어가 죽었을 때
+        foreach(Character player in deadPlayer){
+            resultText.text = "YOU LOSE!";
+        }
+    }
+    
+    //플레이어가 한 명 남았을 때 그라운드를 끝냄.
+    public void FinalWinner(){
+        if(alivePlayer.Count == 1){
+            //각 플레이어들에게 메시지를 보냄.
+            BattleOverMessage();
+        }
+    }
 
 }
