74ea459b55172aace6031d6ebbfba73459f9b85c
Hyeonjun <wns8363@gmail.com>
2022-06-26 12:39:56 +0900

    [Feat] 방 생성 기능 추가

--- Assets/2.Scripts/6.Lobby/CreateRoomPanel.cs
+++ Assets/2.Scripts/6.Lobby/CreateRoomPanel.cs
@@ -1,13 +1,66 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+using Michsky.UI.ModernUIPack;
 
 public class CreateRoomPanel : MonoBehaviour
 {
-    public TMP_InputField roomNameInputField;
-    public TMP_InputField maxPlayersInputField;
+
+    [Header("Entry")]
+    public GameObject pwEntry;
+
+
+    [Header("Input Field")]
+    public CustomInputField roomName;
+    public CustomInputField roomMaxPeople;
+    public CustomInputField roomPassword;
+    public CustomDropdown dropdownMode;
+    public CustomDropdown dropdownMap;
+
+    public Sprite dropdownItemIcon;
+
+    private void Start()
+    {
+        dropdownMode.dropdownItems.Clear();
+        dropdownMap.dropdownItems.Clear();
+        for (int i = 0; i<(int)ModeType.End; i++)
+        {
+            dropdownMode.CreateNewItemFast(GameData.GetMode((ModeType)i), dropdownItemIcon);
+        }
+        dropdownMode.SetupDropdown();
+        for (int i = 0; i < (int)MapType.End; i++)
+        {
+            dropdownMap.CreateNewItemFast(GameData.GetMap((MapType)i), dropdownItemIcon);
+        }
+        dropdownMap.SetupDropdown();
+    }
+    IEnumerator InitRoutine()
+    {
+        yield return null;
+        roomName.inputText.text = $"{PhotonNetwork.LocalPlayer.NickName}의 방";
+        roomName.UpdateState();
+        roomMaxPeople.inputText.text = "4";
+        roomMaxPeople.UpdateState();
+
+        pwToggle.isOn = false;
+        dropdownMode.selectedItemIndex = 0;
+        dropdownMap.selectedItemIndex = 0;
+    }
+
+    private void OnEnable()
+    {
+        // 기본 값 셋팅
+        StartCoroutine("InitRoutine");
+
+    }
+    
+
+    [Header("Toggle")]
+    public Toggle pwToggle;
 
     public void OnCreateRoomCancelButtonClicked()
     {
@@ -16,15 +69,38 @@ public class CreateRoomPanel : MonoBehaviour
 
     public void OnCreateRoomConfirmButtonClicked()
     {
-        string roomName = roomNameInputField.text;
+        int roomNum = PhotonNetwork.CountOfRooms;
+        roomNum++;
 
-        if (roomName == "")
-            roomName = "Room" + Random.Range(1000, 10000);
+        RoomOptions options = new RoomOptions { MaxPlayers = (byte)int.Parse(roomMaxPeople.inputText.text) };
+        options.CustomRoomProperties = new Hashtable {
+            {GameData.ROOM_NAME, roomName.inputText.text },
+            { GameData.ROOM_ISACTIVE_PW, pwToggle.isOn },
+            { GameData.ROOM_PW, pwToggle ? roomPassword.inputText.text : "" },
+            { GameData.GAME_MODE, dropdownMode.selectedItemIndex },
+            { GameData.GAME_MAP, dropdownMap.selectedItemIndex }
+        };
+        options.CustomRoomPropertiesForLobby = new string[]
+        {
+            GameData.ROOM_NAME,
+            GameData.ROOM_ISACTIVE_PW,
+            GameData.ROOM_PW,
+            GameData.GAME_MODE,
+            GameData.GAME_MAP,
+        };
 
-        byte maxPlayer = byte.Parse(maxPlayersInputField.text);
-        maxPlayer = (byte)Mathf.Clamp(maxPlayer, 1, 15);
+        PhotonNetwork.CreateRoom(roomNum.ToString(), options, null);
+    }
 
-        RoomOptions options = new RoomOptions { MaxPlayers = maxPlayer };
-        PhotonNetwork.CreateRoom(roomName, options, null);
+    public void ChangePwToggle()
+    {
+        if (pwToggle.isOn == true)
+        {
+            pwEntry.SetActive(true);
+        }
+        else
+        {
+            pwEntry.SetActive(false);
+        }
     }
 }
